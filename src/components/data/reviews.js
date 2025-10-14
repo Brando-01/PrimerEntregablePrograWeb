9ed82e65322ba57c7a99c6f25009bcb9fbcec7db
Brandon Jonathan Beteta Paucar <20223208@aloe.ulima.edu.pe>
2025-10-13 19:23:20 -0500

    Reseñas publicas

---
+++ src/components/data/reviews.js
@@ -0,0 +1,25 @@
+export const gameReviews = {
+  2: [
+    {
+      name: 'FighterFan',
+      comment: 'Los fatalities son las mejores animaciones que he visto en un juego de lucha. ¡Brutal!',
+      stars: 5,
+      date: '2023-04-10'
+    },
+    {
+      name: 'KombatKing',
+      comment: 'Gran historia, combos adictivos. Solo quisiera que mejoraran el matchmaking.',
+      stars: 4,
+      date: '2023-05-22'
+    }
+  ],
+  4: [
+    {
+      name: 'GeraltFan',
+      comment: 'Uno de los mejores RPG de todos los tiempos. Historia, música y jugabilidad impecables.',
+      stars: 5,
+      date: '2022-11-03'
+    }
+  ]
+  
+};
