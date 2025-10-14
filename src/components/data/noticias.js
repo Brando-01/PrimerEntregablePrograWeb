76ce1cfe497e461e0ea6f8b8d139a0597fd53b1d
Brandon Jonathan Beteta Paucar <20223208@aloe.ulima.edu.pe>
2025-10-13 22:41:14 -0500

    Noticias pantalla principal

---
+++ src/components/data/noticias.js
@@ -0,0 +1,19 @@
+// Archivo convertido a JavaScript
+export const noticiasIniciales = [
+  {
+    id: '1',
+    title: '¡Nuevo juego lanzado!',
+    content: 'Contenido de la noticia sobre el nuevo juego.',
+    fecha: '10/5/2025',
+    estado: 'Publicada',
+    image: '/img/carrusel1.jpg'
+  },
+  {
+    id: '2',
+    title: 'Descuentos de verano',
+    content: 'Contenido de la noticia de descuentos de verano.',
+    fecha: '01/5/2025',
+    estado: 'Publicada',
+    image: '/img/carrusel2.jpg'
+  }
+];
