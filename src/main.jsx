59ea83eb27856f8a7abe4e05efe120d6ded57196
Brandon Jonathan Beteta Paucar <20223208@aloe.ulima.edu.pe>
2025-10-13 19:05:11 -0500

    // main.jsx - VERSIÓN CORREGIDA import React from 'react'; import ReactDOM from 'react-dom/client'; import MainRouter from './MainRouter'; import { OrderProvider } from './components/context/OrderContext'; import { GameProvider } from './components/context/GameContext'; import { UserProvider } from './components/context/UserContext'; import 'bootstrap/dist/css/bootstrap.min.css'; import 'bootstrap/dist/js/bootstrap.bundle.min.js';
    console.log('� Iniciando aplicación...');
    
    ReactDOM.createRoot(document.getElementById('root')).render(
      <React.StrictMode>
        <OrderProvider>
          <GameProvider>
            <UserProvider>
              <MainRouter />
            </UserProvider>
          </GameProvider>
        </OrderProvider>
      </React.StrictMode>
    );

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// main.jsx - VERSIÓN CORREGIDA
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MainRouter from './MainRouter';
+import { OrderProvider } from './components/context/OrderContext';
+import { GameProvider } from './components/context/GameContext';
+import { UserProvider } from './components/context/UserContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+console.log('🚀 Iniciando aplicación...');
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OrderProvider>
+      <GameProvider>
+        <UserProvider>
+          <MainRouter />
+        </UserProvider>
+      </GameProvider>
+    </OrderProvider>
+  </React.StrictMode>
+);
